384033af55f23e46dfb4ab193c14fd3fd3319132
fhrlzmn <fhrlzmn26@gmail.com>
2024-07-06 16:36:58 +0700

    feat: update penduduk data-table column

--- app/admin/penduduk/columns.tsx
+++ app/admin/penduduk/columns.tsx
@@ -14,32 +14,49 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
 import { Penduduk } from '@prisma/client';
+import { formatDate } from '@/lib/utils';
 
 export const columns: ColumnDef<Penduduk>[] = [
+  {
+    accessorKey: 'nik',
+    header: 'NIK',
+    cell: ({ row }) => (
+      <Link
+        href={`/admin/penduduk/${row.original.nik}`}
+        className='hover:text-primary'
+      >
+        {row.original.nik}
+      </Link>
+    ),
+  },
   {
     accessorKey: 'nama',
     header: 'Nama',
   },
   {
-    accessorKey: 'nik',
-    header: 'NIK',
-  },
-  {
     accessorKey: 'jenisKelamin',
     header: 'Jenis Kelamin',
+    accessorFn: (penduduk) =>
+      penduduk.jenisKelamin === 'L' ? 'Laki-laki' : 'Perempuan',
   },
   {
     accessorKey: 'namaAyah',
     header: 'Nama Ayah',
   },
   {
+    accessorKey: 'tanggalLahir',
+    header: 'Tanggal Lahir',
+    accessorFn: (penduduk) => formatDate(penduduk.tanggalLahir),
+  },
+  {
     id: 'actions',
+
     cell: ({ row }) => {
       const penduduk = row.original;
 
       return (
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger asChild className='w-4 text-end'>
             <Button aria-haspopup='true' size='icon' variant='ghost'>
               <MoreHorizontal className='h-4 w-4' />
               <span className='sr-only'>Toggle menu</span>
