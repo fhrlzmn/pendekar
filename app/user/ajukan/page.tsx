6e9003c2c58338c9fa1bf1d0998f87d4f2ec1e2b
fhrlzmn <fhrlzmn26@gmail.com>
2024-07-11 19:01:41 +0700

    feat: ajukan permohonan baru page

---
+++ app/user/ajukan/page.tsx
@@ -0,0 +1,45 @@
+import JenisSuratCard from '@/components/jenis-surat-card';
+
+export default function Page() {
+  return (
+    <div className='w-full min-h-full flex flex-col gap-y-4'>
+      <div className='flex justify-between'>
+        <h1 className='text-xl md:text-2xl font-semibold'>
+          Ajukan Permohonan Surat Baru
+        </h1>
+      </div>
+      <div className='rounded-sm grid grid-cols-3 gap-4'>
+        <div className='grid gap-4'>
+          <JenisSuratCard
+            path='/user/ajukan/sktm'
+            title='Surat Keterangan Tidak Mampu'
+          />
+          <JenisSuratCard
+            path='/user/ajukan/skbn'
+            title='Surat Keterangan Beda Nama'
+          />
+        </div>
+        <div className='grid gap-4'>
+          <JenisSuratCard
+            path='/user/ajukan/skklh'
+            title='Surat Keterangan Kelahiran'
+          />
+          <JenisSuratCard
+            path='/user/ajukan/skkmt'
+            title='Surat Keterangan Kematian'
+          />
+        </div>
+        <div className='grid gap-4'>
+          <JenisSuratCard
+            path='/user/ajukan/sku'
+            title='Surat Keterangan Usaha'
+          />
+          <JenisSuratCard
+            path='/user/ajukan/sik'
+            title='Surat Izin Keramaian'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/jenis-surat-card.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { MailPlus } from 'lucide-react';
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card';
+
+interface JenisSuratCardProps {
+  path: string;
+  title: string;
+}
+
+export default function JenisSuratCard({ path, title }: JenisSuratCardProps) {
+  return (
+    <Link href={path} className='group'>
+      <Card className='group-hover:bg-primary flex flex-col justify-center items-center'>
+        <CardHeader className='text-center'>
+          <CardTitle className='group-hover:text-background'>{title}</CardTitle>
+        </CardHeader>
+        <CardContent className='group-hover:text-background'>
+          <MailPlus className='h-16 w-16 opacity-60' />
+        </CardContent>
+        <CardFooter className='group-hover:text-background'>
+          <p>Ajukan Baru</p>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
