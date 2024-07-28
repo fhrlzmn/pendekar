8f36cbe4d6ee9b7db1792e7bb836669496824120
fhrlzmn <fhrlzmn26@gmail.com>
2024-07-28 18:49:14 +0700

    fix: minimum 6 char password for admin login

--- schema/login.ts
+++ schema/login.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const adminLoginSchema = z.object({
   username: z.string().min(3, 'Username harus lebih dari 3 karakter'),
-  password: z.string().min(8, 'Password harus lebih dari 8 karakter'),
+  password: z.string().min(6, 'Password harus lebih dari 6 karakter'),
 });
 
 export const userLoginSchema = z.object({
